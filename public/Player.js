d8b6b341e6e5783f6c6791bfafaa243a23e4097d
AnaGciaSchz <anags.clm@gmail.com>
2020-12-29 12:58:23 +0100

    Quitando código que sobra

--- public/Player.js
+++ public/Player.js
@@ -1,18 +1,6 @@
 class Player {
   constructor(player) {
     this.id = player.id;
-    this.sounds = [];
-  }
-
-  addSound(sound){
-    this.sounds.push(sound);
-  }
-
-  getSound(){
-    if(sounds.length>0){
-      return this.sounds.shift();
-    }
-    return null;
   }
 
 }
